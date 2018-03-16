f730057a6fc740c9b04877ca9cf810a605285018
unknown <Satish@Satish-PC.RIVERBRIDGE.COM>
2018-03-16 17:32:32 +0530

    map added

--- src/pages/google/google.ts
+++ src/pages/google/google.ts
@@ -34,24 +34,23 @@ export class GooglePage {
    types.forEach(element => {
    
   	this.http.get('https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=19.0049004,72.8236264&radius=500&type='+element+'&key=%20AIzaSyBg1KqM98DIC8TA1ngpS3luuP1A-_aQsfg').map(res => res.json()).subscribe(data => {
-		    console.log(data);
-		     console.log(data.status);
-		      console.log(data.results);
+		    // console.log(data);
+		    //  console.log(data.status);
+		    //   console.log(data.results);
 		      Object.keys(data.results).forEach(key=> {
-                console.log(data.results[key]['geometry']['location'])  ;     
 
-                let lat ={"lat":data.results[key]['geometry']['location'].lat,"lng":data.results[key]['geometry']['location'].lng,'type':element,'name':data.results[key]['name']};
+                // console.log(data.results[key])  ;     
+
+                let lat ={"lat":data.results[key]['geometry']['location'].lat,"lng":data.results[key]['geometry']['location'].lng,'type':element,'name':data.results[key]['name'],'icon':data.results[key]['icon']};
 
                 this.locations.push(lat);
             });
 
 		});
 
-  		console.log(this.locations);
-  		 console.log("===========================");
-  		 console.log(element);
-  		 console.log("===========================");
 		});
+
+   console.log(this.locations);
   }
 
   ionViewDidLoad() {
@@ -64,10 +63,10 @@ export class GooglePage {
     let mapOptions: GoogleMapOptions = {
       camera: {
         target: {
-          lat: 43.0741904,
-          lng: -89.3809802
+          lat: 19.07598,
+          lng: 72.87766
         },
-        zoom: 18,
+        zoom: 25,
         tilt: 30
       }
     };
@@ -81,21 +80,37 @@ export class GooglePage {
         console.log('Map is ready!');
 
         // Now you can use all methods safely.
-        this.map.addMarker({
-            title: 'Ionic',
-            icon: 'blue',
-            animation: 'DROP',
-            position: {
-              lat: 43.0741904,
-              lng: -89.3809802
-            }
-          })
-          .then(marker => {
-            marker.on(GoogleMapsEvent.MARKER_CLICK)
-              .subscribe(() => {
-                alert('clicked');
-              });
-          });
+
+        for(let k=1 ;k<this.locations.length;k++){
+
+            let image = {
+                url: this.locations[k].icon,
+                size: {
+                  width: 25,
+                  height: 25
+                }
+                // This marker is 20 pixels wide by 32 pixels high.
+               
+              };
+
+            let loc = this.locations[k];
+            this.map.addMarker({
+              title: loc.name,
+              icon: image,
+              animation: 'DROP',
+              position: {
+                lat: loc.lat,
+                lng: loc.lng
+              }
+            })
+            .then(marker => {
+              marker.on(GoogleMapsEvent.MARKER_CLICK)
+                .subscribe(() => {
+                  alert('clicked');
+                });
+            });
+        }
+        
 
       });
   }
